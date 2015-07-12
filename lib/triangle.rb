4e411d399faf8b5280919e4ff8bf0b2f3fe4438d
mbrickenworthe <adamwinters456@gmail.com>
2015-07-12 12:54:55 -0400

    Adding a custom error message to a OO ruby class

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
-  # write code here
+  attr_reader :side1, :side2, :side3, :trianleerror, :group, :standard
+    
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    if side1 == 0 || side2 == 0 || side3 == 0 || (side3 + side2) <= side1 || (side3 + side1) <= side2 || (side1 + side2) <= side3
+      raise TriangleError 
+    elsif side3 == side2 && side3 == side1 && side1 == side2
+      return :equilateral
+    elsif (side3 == side2 ) || (side2 == side1) || (side3 == side1)
+      return :isosceles
+    else 
+      return :scalene
+    end
+  end
+end
+
+class TriangleError < StandardError
+  
+  def initialize
+     puts "That ain't a correct triangle, ya fool!"
+  end
+
 end
