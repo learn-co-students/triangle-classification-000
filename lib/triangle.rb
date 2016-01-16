3e2e29f5ea67251d88f6905dcb95df055fe28f93
NoneOfMaster <rcraigt@gmail.com>
2016-01-15 21:11:42 -0800

    blankets. how do they work?

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,25 @@
 class Triangle
-  # write code here
+
+  def initialize(a, b, c)
+    @sides = (@a, @b, @c = a, b, c)
+  end
+
+  def kind
+      if @sides.select{|s| s <= 0}.count > 0 || @a >= (@b + @c) || @c >= (@b + @a) || @b >= (@c + @a)
+        raise TriangleError
+      elsif @sides.select{|side| side == @a}.count == 3
+        :equilateral
+      elsif !@sides.select{|side| @sides.select{|s| s == side}.count == 2}.empty?
+        :isosceles
+      else
+        :scalene
+      end
+  end
+
+end
+ 
+class TriangleError < StandardError
+  def message
+    "Those sides don't make a triagle"
+  end
 end
